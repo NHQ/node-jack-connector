ae284f8f6e7c9aed32c16a274aec8e50ca951b9a
johnnyscript <mostmodernist@gmail.com>
2015-07-04 00:20:37 -0700

    whammie

--- examples/entry.js
+++ examples/entry.js
@@ -2,45 +2,113 @@ var debounce = require('debounce')
 sp = require('serialport')
 SP = sp.SerialPort
 port = new SP('/dev/ttyACM0')
+var midi = require('midi')
+var input = new midi.input()
 
+input.openPort(1)
 
-//var midi = require('midi')
-//var input = new midi.input()
-//input.openPort(1) // probably
-
-//input.on('message', function(m){
-//console.log(m.oString())
-//})
 var jsynth = require('./jacksynth')
 var sr = jsynth.sampleRate
 var jdelay = require('jdelay')
-var bpm =  115
+var bpm =  151
 var secpb = bpm / 60
 var spb = sr * 60 / bpm 
-
-console.log('spb %d', spb)
-var map = [ 2, 4]
+var dmod = 1//Math.PI / 4
+var dmodifier = 1/4
+var dmix = 1
+var pressMods = {}
+pressMods.pedal1 = false
+var durr = 3
+var fbmod =  1
+var map = [ 2, 4, 8]
 
 var delayos = []
 
 var setDelays = function(){
   delayos = map.map(function(e, i){
-     return jdelay(Math.floor(spb * e  * 2), e / (delayos.length * 4), (e / delayos.length * 4), i / map.length)
+     return jdelay(Math.floor(spb * (e) * dmod), (( 1 + i) / delayos.length) * fbmod, dmix)
   })
 }
+
+input.on('message', function(d, m){
+  var data = m.toString().split(',')
+  //console.log(data)
+  if(data[0] === '176'){
+    if(data[1].match('67')){
+      pressMods[data[1]] = data[2] === '127' ? true : false    
+      console.log(pressMods)
+      return
+    }
+     
+    switch(data[1]){
+      case '1':
+        if(pressMods['67']){
+          map[0] += data[2] === '127' ? -.1 : .1
+          console.log('map[0] = %d', map[0])
+        }
+        else {
+          fbmod += Number(data[2]) > 2 ? -.01 : .01
+          console.log('fbmod = %d', fbmod)
+        }
+      break
+      case '2':
+        if(pressMods['67']){
+          map[1] += data[2] === '127' ? -.1 : .1
+          console.log('map[1] = %d', map[1])
+        }
+        else{
+          var isd = dmod <= 2
+          var mod
+          if(isd) {
+            mod = .01 
+            dmod += Number(data[2]) > 2 ? -mod : mod
+          }
+          else{
+            dmod += Number(data[2]) > 2 ? -dmodifier : dmodifier
+          }
+          console.log('delay beat mod = %d', dmod)
+        }
+      break
+      case '3':
+        if(pressMods['67']){
+          map[2] += data[2] === '127' ? -.1 : .1
+          console.log('map[2] = %d', map[2])
+        }
+        else{
+          bpm += Number(data[2]) > 2 ? -1 : 1
+          secpb = bpm / 60
+          spb = sr * 60 / bpm 
+          console.log('bpm = %d', bpm)
+        }
+      break
+      case '4':
+        dmix += Number(data[2]) > 2 ? -0.01 : 0.01
+        console.log('dmix = %d', dmix)
+      break
+    }
+  }
+})
+
 
 setDelays()
 
 var mix = true
 function getDelays(i){
   if(mix){
-    return   ( i + delayos.reduce(function(a, delay, y){
-      return  (delay(a, Math.floor(spb * map[y] * 2), map[y] / (delayos.length * 4), 1 - y / delayos.length)) // delayos.length 
-    }, i) ) 
+    if(pressMods['pedal1']){
+      return   ( i + delayos.reduce(function(a, delay, y){
+        return    a + (delay(i, Math.floor(spb * (map[y]) * dmod),  ((1 + y) / delayos.length) * fbmod, dmix)) / 2 // delayos.length 
+      }, 0) ) 
+    }
+    else{
+      return   ( i + delayos.reduce(function(a, delay, y){
+        return    (delay(a, Math.floor(spb * (map[y]) * dmod),  ((1 + y) / delayos.length) * fbmod, dmix)) / 2 // delayos.length 
+      }, i) )
+    }
   }
   else{
     return  ( i + delayos.reduce(function(a, delay, y){
-      return  a + (delay(0, Math.floor(spb * map[y] * 2), 1,  1 - y / delayos.length)) // delayos.length 
+      return    a + (delay(0, Math.floor(spb * (map[y]) * dmod), ((1 + y) / delayos.length) * fbmod,  dmix)) / 2// delayos.length 
     }, 0) ) 
   }
 }
@@ -106,19 +174,21 @@ function button(i){
 
       break
       case 1:
+        pressMods['pedal1'] = !pressMods['pedal1']
+        console.log('hobble mode = %s', !pressMods['pedal1'])
         // what
       break
       case 2:
         //  override delays
         mix = !mix
-      console.log(mix)
+        console.log('playover mode = %s', mix)
       break
       case 3:
         //  clear delays
         setDelays()
       break
     }
-    console.log(i, data)
+    //console.log(i, data)
   }, 100, true)
 }
 
